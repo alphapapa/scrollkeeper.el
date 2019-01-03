954c56b7aaf016ad9759c07a4fa1a93ebb1d49ce
Adam Porter <adam@alphapapa.net>
2019-01-03 04:47:58 -0600

    Change: Target Emacs 25.1+

--- scrollkeeper.el
+++ scrollkeeper.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/alphapapa/scrollkeeper.el
 ;; Keywords: convenience
 ;; Version: 0.1-pre
-;; Package-Requires: ((emacs "24"))
+;; Package-Requires: ((emacs "25.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
